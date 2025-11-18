1d64264a83b6512652959113ca25d76d4c59aaff
machiechie <heyderli_005@mail.ru>
2025-11-18 19:54:16 +0000

    dasaf

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) = 0:
+        first_word ="None"
+    else:
+        first_word = sentence[1]
+    return(len(sentence), first_word)
