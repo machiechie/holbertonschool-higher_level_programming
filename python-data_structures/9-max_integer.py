0ed4c86eea68ea869ef27f5a954d5ac5307e8948
machiechie <heyderli_005@mail.ru>
2025-11-18 20:14:14 +0000

    hshsj

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        print("None")
-    else:
-        maxs = sort(my_list)
-        return(maxs[-1])
-        
+        return None
+    new_list = my_list.copy()
+    new_list.sort()
+    return new_list[-1]  
