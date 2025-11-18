37884ff8e051d3469a132074b3e59cf98176b36b
machiechie <heyderli_005@mail.ru>
2025-11-18 19:59:14 +0000

    dasaf

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,4 +4,4 @@ def multiple_returns(sentence):
         first_word ="None"
     else:
         first_word = sentence[0]
-        print("Length: {} - First character: {}".format(len(sentence), first_word)
+        return(len(sentence), first_word)
