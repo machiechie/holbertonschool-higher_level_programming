fca3d2894f81390d16860aff9fbbb258169291d5
machiechie <heyderli_005@mail.ru>
2025-11-19 08:09:01 +0000

    sfS

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_val = None
+    max_key = None
+    for key, value in a _dictionary:
+        if value > max_val:
+            max_val = value
+            max_key = key
+    return max_key
