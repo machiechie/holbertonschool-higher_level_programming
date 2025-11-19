4152637de4e86388d3724bcb9efef12863853028
machiechie <heyderli_005@mail.ru>
2025-11-19 06:58:20 +0000

    sdfF

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted = a_dictionary.sort()
-    return sorted
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary)
