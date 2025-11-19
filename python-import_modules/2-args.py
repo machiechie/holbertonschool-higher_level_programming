74191467c00ec562892d3ada5dcf69ee86961440
machiechie <heyderli_005@mail.ru>
2025-11-19 11:07:51 +0000

    sfdf

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arg_count = len(argv) - 1°
-    if arg_count == 0:
-        print("0 arguments.")
-    elif arg_count == 1:
-        print("1 argument:")
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(arg_ount))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(i))
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
