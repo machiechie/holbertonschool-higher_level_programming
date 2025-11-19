cb1192b1d9ca929a029ac3a268d19747e408fdfd
machiechie <heyderli_005@mail.ru>
2025-11-19 06:35:54 +0000

    sdfF

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = []
-    for el1 in set_1:
-        newest_set = new_set.append(el1)
+    for el in set_1:
+        if el not in new_set:
+            new_set.append(el)
     for el in set_2:
-        if el2 not in newest_set:
-            newest_set.append(el2)
-    return newest_set
+        if el not in new_set:
+            new_set.append(el)
+    return new_set
