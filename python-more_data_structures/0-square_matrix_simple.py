7e330401314afd225245db8eb003dc744102265e
machiechie <heyderli_005@mail.ru>
2025-11-19 05:28:58 +0000

    sdfdf

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix=[]
     for row in matrix:
         new_row=[]
-        for num in matrix[row]:
+        for num in row:
             new_row.apped(num * num)
         new_matrix.append(new_row)
     return new_matrix
