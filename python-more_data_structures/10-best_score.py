951bd214a589f01d78f60795610681711cf8a0e5
machiechie <heyderli_005@mail.ru>
2025-11-19 08:10:29 +0000

    sfS

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max_val = None
     max_key = None
-    for key, value in a _dictionary:
+    for key, value in a _dictionary.items():
         if value > max_val:
             max_val = value
             max_key = key
