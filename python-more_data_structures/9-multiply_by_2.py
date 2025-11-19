20b5b94b29d4c0c0d8c60326fae35448d3e07122
machiechie <heyderli_005@mail.ru>
2025-11-19 07:58:39 +0000

    SSf

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = []
-    for i in a_dictionary:
-        i = i ** 2
-        new_dic.append(i)
+    new_dic = {}
+    for key, value in a_dictionary.items():
+        new_dic[key] = value * 2
     return new_dic
