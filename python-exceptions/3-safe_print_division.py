6c2b4814ecf2d9af4e765966b55e01f7a88fa9c3
machiechie <heyderli_005@mail.ru>
2025-11-21 09:08:01 +0000

    agag

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    divv = None
+    try:
+        divv = a / b
+    except ZeroDivisionError:
+        divv = None
+    finally:
+        print("Inside result:{}".format(divv))
+        return result
