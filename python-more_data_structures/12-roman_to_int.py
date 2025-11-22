ba2dabd1342a810daa3295d9b1ffe6da508f7047
machiechie <heyderli_005@mail.ru>
2025-11-22 13:30:25 +0000

    sdsdf

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python3
-
+#!/usr/bin/pytHON3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
 
-if not isinstance(roman_string, str) or roman_string is None:
-    return 0
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
-roman_map = {
-    'I': 1, 'V': 5, 'X': 10, 'L': 50,
-    'C': 100, 'D': 500, 'M': 1000
-}
+    result = 0
+    prev_value = 0
 
-total = 0
-prev_value = 0
+    for letter in reversed(roman_string):
+        value = roman_dict.get(letter, 0)
+        if value >= prev_value:
+            result += value
+        else:
+            result -= value
+        prev_value = value
 
-for char in reversed(roman_string):
-    value = roman_map.get(char, 0)
-    if value < prev_value:
-        total -= value
-    else:
-        total += value
-    prev_value = value
-
-return total
+    return result
