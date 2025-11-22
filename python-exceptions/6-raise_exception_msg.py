4f4db3e472624ef6db4b81f4f4d5d25627a48342
machiechie <heyderli_005@mail.ru>
2025-11-22 10:39:47 +0000

    agag

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     try:
-        raise NameError(message)
-    except NameError:
-        print("Python is cool")
+        raise_exception_msg("C is fun")
+    except NameError as e:
+        print(e)
