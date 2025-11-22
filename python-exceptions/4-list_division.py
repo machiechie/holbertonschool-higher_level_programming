2f487d8a70391b3c293a58de09e5a9e5d883529d
machiechie <heyderli_005@mail.ru>
2025-11-22 10:12:31 +0000

    agag

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(new_list):
+    for i in range(list_length):
         try:
             a = my_list_1[i]
             b = my_list_2[i]
