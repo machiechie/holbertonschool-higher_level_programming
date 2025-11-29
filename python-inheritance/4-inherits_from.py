110f79053b421df372f57133abc35bb2a4331217
machiechie <heyderli_005@mail.ru>
2025-11-29 14:58:23 +0000

    commit message

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a function to check class inheritance."""
 
+
 def inherits_from(obj, a_class):
     """Function that returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class ;
