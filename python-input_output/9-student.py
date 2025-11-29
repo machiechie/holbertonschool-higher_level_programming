74224d3896b940b2aacfff6921813a31fac216d3
machiechie <heyderli_005@mail.ru>
2025-11-29 20:27:14 +0000

    commit message

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,6 +7,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
- 
-    def to_json(self):
+        
+    def to_json(self)
         return self.__dict__
