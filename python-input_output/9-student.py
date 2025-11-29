83d4d5f05f609cdb635d8932a274be64b26740c8
machiechie <heyderli_005@mail.ru>
2025-11-29 20:27:53 +0000

    commit message

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,6 +7,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
-    def to_json(self)
+
+    def to_json(self):
+        """Returns the dictionary representation of a Student."""
         return self.__dict__
