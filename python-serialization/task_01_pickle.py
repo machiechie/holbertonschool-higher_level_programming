9348e107b418d7031b8970cae582e1cb3f2d0c83
machiechie <heyderli_005@mail.ru>
2025-11-29 20:50:49 +0000

    commit message

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module that defines a custom class with serialization and deserialization methods using pickle."""
+
+import pickle
+
+class CustomObject:
+    def __init__(self, name: str, age: int, is_student: bool):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename: str):
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+     @classmethod
+    def deserialize(cls, filename: str):
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
