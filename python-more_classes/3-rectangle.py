aed3f2944ee2a104d2e8f211a1cbd78cfc8db1f3
machiechie <heyderli_005@mail.ru>
2025-11-29 10:53:28 +0000

    commit message

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -35,12 +35,12 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
