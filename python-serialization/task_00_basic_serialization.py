8bc4651532c02784eafaf84ff46667075101c5b8
machiechie <heyderli_005@mail.ru>
2025-11-29 20:43:47 +0000

    commit message

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for serializing and deserializing data to and from JSON files."""
+
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize data to JSON and save it to a file."""
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+def load_and_deserialize_from_file(filename):
+    """Load data from a file and deserialize it from JSON."""
+    with open(filename, 'r') as f:
+        return json.load(f)
