7190fcb94d46948303df8b2f05c93b13893a079b
machiechie <heyderli_005@mail.ru>
2025-11-29 19:44:48 +0000

    commit message

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file, using a JSON representation"""
+    import json
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
