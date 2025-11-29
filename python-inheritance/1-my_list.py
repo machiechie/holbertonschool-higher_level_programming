13a1dc653af75b637025c1b102473dbf60adccfc
machiechie <heyderli_005@mail.ru>
2025-11-29 14:22:38 +0000

    commit message

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines the MyList class that inherits from list."""
+
+
+class MyList(list):
+    """Custom list class that inherits from list."""
+
+    def print_sorted(self):
+        """Prints the list in ascending order."""
+        print(sorted(self))
