88b12ca28b727e7c37ef11a19997cb16e48c5462
machiechie <heyderli_005@mail.ru>
2025-11-29 15:47:59 +0000

    commit message

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a square class that inherits from Rectangle."""   
+"""Defines a square class that inherits from Rectangle."""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
