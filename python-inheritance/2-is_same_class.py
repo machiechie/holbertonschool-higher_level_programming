343e6226b433c43e984d21c9bbf74186f6da5b08
machiechie <heyderli_005@mail.ru>
2025-11-29 14:47:54 +0000

    commit message

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,8 +2,8 @@
 """Module for checking if an object
 is exactly an instance of a specified class."""
 
-def is_same_class(obj, a_class):
 
+def is_same_class(obj, a_class):
     """Check if obj is exactly an instance of a_class."""
     if isinstance(a_class, type) and type(obj) is a_class:
         return True
