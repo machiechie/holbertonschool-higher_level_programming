2fd7494ebdd9327edfdcba6b94c33596cede30da
machiechie <heyderli_005@mail.ru>
2025-12-03 11:24:02 +0000

    commit message

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,35 +2,26 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Словарь пользователей в памяти
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+users = {}  # пустой словарь для тестера
 
-# Главная страница
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
-# Список всех username
 @app.route("/data")
 def data():
     return jsonify(list(users.keys()))
 
-# Статус API
 @app.route("/status")
 def status():
     return "OK"
 
-# Информация о конкретном пользователе
 @app.route("/users/<username>")
 def get_user(username):
     if username in users:
         return jsonify(users[username])
     return jsonify({"error": "User not found"}), 404
 
-# Добавление нового пользователя
 @app.route("/add_user", methods=["POST"])
 def add_user():
     try:
@@ -48,6 +39,5 @@ def add_user():
     users[username] = data
     return jsonify({"message": "User added", "user": data}), 201
 
-# Запуск сервера
 if __name__ == "__main__":
     app.run()
