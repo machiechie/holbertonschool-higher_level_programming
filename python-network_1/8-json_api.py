7f4f1e8c1cf31ca1451b0a765990f9e534316d71
machiechie <heyderli_005@mail.ru>
2025-12-03 06:22:31 +0000

    commit message

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a letter to the API and handles JSON response"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
