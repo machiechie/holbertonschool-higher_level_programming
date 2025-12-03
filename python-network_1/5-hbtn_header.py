73da974af4092f450bae05664e649d5ec3e7c66a
machiechie <heyderli_005@mail.ru>
2025-12-03 05:43:56 +0000

    commit message

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Displays the X-Request-Id header from a URL"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
